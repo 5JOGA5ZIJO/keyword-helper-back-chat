bd984c37c5c8d8efd6f3bc179c2285a009784fa8
kim2ju <bamtori05@naver.com>
2023-02-18 01:47:52 +0900

    상태 표시

--- index.js
+++ index.js
@@ -13,34 +13,62 @@ const io = new Server(httpServer, {
     }
 });
 
+let users;
+
+const setting = async() => {
+    [users] = await db.promise().query(`SELECT id FROM users`);
+    users.forEach(x=> {
+        x.status = 'leave'
+    });
+}
+setting();
+
 io.on("connection", (socket) => {
-    socket.on("enter", async (nickname) => {
+    socket.on("enter", async (id, nickname) => {
+        socket["db_id"] = id;
         socket["nickname"] = nickname;
         socket["last_chat_id"] = 0;
         socket.join("room");
-        io.in("room").emit("new_entry", nickname);
 
-        const [user] = await db.promise().query(`SELECT * FROM users WHERE id = '${socket.id}'`);
+        const [chats] = await db.promise().query(`
+            SELECT users.nickname AS 'nickname', chats.chat AS 'chat', chats.created_at AS 'created_at' 
+            FROM chats
+            LEFT JOIN users ON chats.user_id = users.id
+            ORDER BY created_at DESC
+        `);
+        socket.emit("prev_message", chats)
+
+        const [user] = await db.promise().query(`SELECT * FROM users WHERE id = '${id}'`);
         if (user.length == 0) {
-            db.promise().query(`INSERT INTO users (id, nickname) VALUES ('${socket.id}', '${nickname}')`);
-            db.promise().query(`INSERT INTO chats (user_id, chat) VALUES ('system', '${nickname} enter')`);
+            db.promise().query(`INSERT INTO users (id, nickname) VALUES ('${id}', '${nickname}')`);
+            db.promise().query(`INSERT INTO chats (user_id, chat) VALUES ('system', '${nickname}님이 입장하였습니다.')`);
+            users.push({"id": id, "status": 'connect'})
         } else {
-            db.promise().query(`UPDATE users SET nickname = '${nickname}' WHERE id = '${socket.id}'`);
-            db.promise().query(`SELECT last_chat_id FROM users WHERE id = '${socket.id}'`);
+            db.promise().query(`UPDATE users SET nickname = '${nickname}' WHERE id = '${id}'`);
+            const [last_chat_id] = await db.promise().query(`SELECT last_chat_id FROM users WHERE id = '${id}'`);
+            socket["last_chat_id"] = last_chat_id[0].last_chat_id;
+            users.forEach(x => {
+                if (x.id == id) x.status = 'connect'
+            });
         }
+        io.in("room").emit("new_entry", nickname, users);
+        console.log(users);
     });
 
     socket.on("new_message", (message) => {
         io.in("room").emit("new_message", `'${socket.nickname}': '${message}'`);
         console.log(`'${socket.nickname}': '${message}'`);
-        db.promise().query(`INSERT INTO chats (user_id, chat) VALUES ('${socket.id}', '${message}')`);
+        db.promise().query(`INSERT INTO chats (user_id, chat) VALUES ('${socket.db_id}', '${message}')`);
     });
 
     socket.on("disconnecting", async () => {
-        socket.to("room").emit("new_leave", socket.nickname);
+        users.forEach(x => {
+            if (x.id == socket.db_id) x.status = 'leave'
+        });
+        io.to("room").emit("new_leave", socket.nickname, users);
         const [last_chat] = await db.promise().query(`SELECT id FROM chats ORDER BY id DESC LIMIT 1`);
         socket["last_chat_id"] = last_chat[0].id;
-        db.promise().query(`UPDATE users SET last_chat_id = '${socket.last_chat_id}' WHERE id = '${socket.id}'`);
+        db.promise().query(`UPDATE users SET last_chat_id = '${socket.last_chat_id}' WHERE id = '${socket.db_id}'`);
     });
   
 });
